c78c044b7f114c45671191b2029fe7e1aaf237a3
Michael Geiger <michael.k.geiger@gmail.com>
2026-07-09 19:48:01 -0700

    Initial Commit

---
+++ StarTime/StarTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarTimeApp.swift
+//  StarTime
+//
+//  Created by Michael Geiger on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct StarTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarTimeTests/StarTimeTests.swift
@@ -0,0 +1,19 @@
+//
+//  StarTimeTests.swift
+//  StarTimeTests
+//
+//  Created by Michael Geiger on 7/9/26.
+//
+
+import Testing
+@testable import StarTime
+
+struct StarTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
